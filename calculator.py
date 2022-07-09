21a37aa61246f60a19184020c192b21aea49bdf4
Viru9029 <vbkate9@gmail.com>
2022-07-09 21:51:15 +0530

    chnaged

---
+++ calculator.py
@@ -0,0 +1,11 @@
+print('Name : Kate Virendra Bhagwan')
+print('CDAC NOIDA')
+print('PG-DAI')
+print('MARCH-22')
+print('PRN No : 220320528039')
+
+print('Addition : ',20+25)
+print('Subtraction : ', 50-15)
+print('Multiplication : ',13*4)
+print('Division : ',60/7)
+print('Pi Value : ',3.14)
